7ab7dfda578fb8a7367a4f7cb5af6c6e036a9836
subiczadrian <subiczadrian@gmail.com>
2023-04-02 22:17:53 +0200

    sign up success, login and connect to mysql db

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $mysqli = require __DIR__ . '/database.php';
+
+        $sql = sprintf('SELECT * FROM user WHERE username = "%s"', $mysqli -> real_escape_string($_POST["username"]));
+
+        $result = $mysqli -> query($sql);
+
+        $user = $result -> fetch_assoc();
+
+        var_dump($user);
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Regisztráció</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <h1>Bejelentkezés</h1>
+
+      <form id="login-form" method="post">
+      <div class="mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="username-input"
+            placeholder="Felhasználónév"
+            name="username"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            class="form-control"
+            id="password-input"
+            placeholder="Jelszó"
+            name="password"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+      </form>
+    </div>
+  </body>
+</html>
--- process-sign-up.php
+++ process-sign-up.php
@@ -1,7 +1,20 @@
 <?php
-    print_r($_POST);
-
     $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-    var_dump($password_hash);
+    $mysqli = require __DIR__ . '/database.php';
+
+    $sql = 'INSERT INTO user (username, email, password_hash) VALUES (?, ?, ?)';
+
+    $stmt = $mysqli -> stmt_init();
+
+    if(!$stmt -> prepare($sql)){
+        die("SQL error: " . $mysqli ->error);
+    }
+   
+    $stmt ->bind_param('sss', $_POST["username"], $_POST["email"], $password_hash);
+
+    $stmt -> execute();
+    
+    header('Location: sign-up-success.html');
+    exit;
 ?>
