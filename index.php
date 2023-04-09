d21561922484c7982a31826b7c41d61c395fb493
subiczadrian <subiczadrian@gmail.com>
2023-04-09 14:00:53 +0200

    logout

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
-    session_start();
+session_start();
 
-    print_r($_SESSION);
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
 
 ?>
 
@@ -16,5 +26,21 @@
 </head>
 <body>
     
+    <h1>Kezdőlap</h1>
+
+    <?php if (isset($user)): ?>
+
+        <p>Hello <?= htmlspecialchars($user["username"]) ?></p>
+
+        <p><a href="logout.php">Kijelentkezés</a></p>
+
+    
+
+    <?php else: ?>
+
+        <p><a href="login.php">Bejelentkezés</a></p>
+
+    <?php endif; ?>
+
 </body>
 </html>
